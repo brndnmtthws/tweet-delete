3599c5941bf51c4eed263983f58c781437c14969
Brenden Matthews <brenden@diddyinc.com>
2019-03-13 22:45:07 -0400

    Bump version for next release.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ test_requires = [
 
 setuptools.setup(
     name="tweet-delete",
-    version="0.1.6",
+    version="0.1.7",
     author="Brenden Matthews",
     author_email="brenden@diddyinc.com",
     description="Self-destructing Tweet tool",
