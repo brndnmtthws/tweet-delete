4d635d7eb82949baa77ea8b6da585f64e464f2ca
Brenden Matthews <brenden@diddyinc.com>
2019-03-14 07:39:44 -0400

    Add missing file.

---
+++ tweet_delete/util.py
@@ -0,0 +1,20 @@
+def td_format(seconds):
+    # Thanks https://stackoverflow.com/a/13756038
+    seconds = int(seconds)
+    periods = [
+        ('year',        60*60*24*365),
+        ('month',       60*60*24*30),
+        ('day',         60*60*24),
+        ('hour',        60*60),
+        ('minute',      60),
+        ('second',      1)
+    ]
+
+    strings = []
+    for period_name, period_seconds in periods:
+        if seconds >= period_seconds:
+            period_value, seconds = divmod(seconds, period_seconds)
+            has_s = 's' if period_value > 1 else ''
+            strings.append("%s %s%s" % (period_value, period_name, has_s))
+
+    return ", ".join(strings)
